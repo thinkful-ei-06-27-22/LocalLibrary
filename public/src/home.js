7bd59d9cb49c0c5a2c63074a7e9543a20d25edec
Terra Taylor <terra.taylor@gmail.com>
2022-07-06 09:17:08 -0500

    init commit

---
+++ public/src/home.js
@@ -0,0 +1,30 @@
+function getTotalBooksCount(books) {
+  return books.length
+}
+
+function getTotalAccountsCount(accounts) {
+  return accounts.length;
+}
+
+function getBooksBorrowedCount(books) {}
+
+function _groupByKey(key){
+
+}
+
+function getMostCommonGenres(books) {
+  _groupByKey(key)
+}
+
+function getMostPopularBooks(books) {}
+
+function getMostPopularAuthors(books, authors) {}
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
